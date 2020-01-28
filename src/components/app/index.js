f0acb66883d5798af277f0ecc3c14e489f0d6fdd
GavinCallander <gavin.callander@gmail.com>
2020-01-28 14:43:46 -0800

    app updated

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,5 +1,7 @@
 // dependencies
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
 // components
 import { SignIn, SignUp } from '../auth';
 // css
@@ -7,8 +9,41 @@ import './style.css';
 
 export const App = () => {
     const [user, setUser] = useState(null);
+    // effect hook
+    useEffect(() => {
+        decodeToken()
+    }, []);
+    // helper functions
+    const updateUser = newToken => {
+        if (newToken) {
+            // token to localStorage
+            localStorage.setItem('mernToken', newToken);
+            decodeToken(newToken);
+        } else {
+            setUser(null);
+        }
+    };
+
+    const decodeToken = existingToken => {
+        let token = existingToken || localStorage.getItem('mernToken');
+        if (token) {
+            let decoded = jwtDecode(token)
+            // if !token or not decodable, not logged in
+            if (!decoded || Date.now() >= decoded.exp * 1000) {
+                console.log('expired');
+                setUser(null);
+            } else {
+                setUser(decoded);
+            }
+        } else {
+            setUser(null);
+        }
+    };
+
     return (
-        <div className='app'>
-        </div>
+        <Router>
+            <div className='app'>
+            </div>
+        </Router>
     )
 };
