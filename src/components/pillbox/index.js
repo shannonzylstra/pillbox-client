d112fbd554a36da7bb373be6d723ffeedfe4cbe8
Gavin Callander <gavincallander@Gavins-MacBook-Pro.local>
2020-01-31 16:47:35 -0800

    trying to fix issue with components

--- src/components/pillbox/index.js
+++ src/components/pillbox/index.js
@@ -1,5 +1,5 @@
 // dependencies
-import React from 'react';
+import React, { useState } from 'react';
 // components
 import { PillboxColumn } from './PillboxColumn';
 import { PillboxLabel } from './PillboxLabel';
@@ -9,18 +9,37 @@ import Paper from '@material-ui/core/Paper';
 import './style.scss';
 
 export const Pillbox = () => {
+    const [mon, setMon] = useState([
+        {
+            name: 'Xanax',
+            times: ['am', 'eve'],
+            condition: 'Anxiety'
+        },
+        {
+            name: 'Immodium',
+            times: ['pm', 'eve'],
+            condition: 'Bad arse'
+        }
+    ]);
+    const [tue, setTue] = useState([]);
+    const [wed, setWed] = useState([]);
+    const [thu, setThu] = useState([]);
+    const [fri, setFri] = useState([]);
+    const [sat, setSat] = useState([]);
+    const [sun, setSun] = useState([]);
+
 
     return (
         <div className='pillbox'>
             <Paper className='pillbox-display'>
                 <PillboxLabel />
-                <PillboxColumn day='Mon' />
-                <PillboxColumn day='Tue' />
-                <PillboxColumn day='Wed' />
-                <PillboxColumn day='Thu' />
-                <PillboxColumn day='Fri' />
-                <PillboxColumn day='Sat' />
-                <PillboxColumn day='Sun' />
+                <PillboxColumn day='Mon' data={mon} />
+                <PillboxColumn day='Tue' data={tue} />
+                <PillboxColumn day='Wed' data={wed} />
+                <PillboxColumn day='Thu' data={thu} />
+                <PillboxColumn day='Fri' data={fri} />
+                <PillboxColumn day='Sat' data={sat} />
+                <PillboxColumn day='Sun' data={sun} />
             </Paper>
         </div>
     )
